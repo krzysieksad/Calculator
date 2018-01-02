36bfd74cd6732ee6d8395ee8836634d2a5db9ffc
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-02 14:41:16 +0100

    Confirmed that code workes

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,4 +1,16 @@
 package com.kodilla.calculator;
 
 public class Calculator {
+    private void addition(final int addendA, final int addendB) {
+        System.out.println("Addition of " + addendA + " and " + addendB + " equals " + (addendA + addendB));
+    }
+    private void subtraction(final int minuend, final int subtrahend) {
+        System.out.println("Subtraction of " + subtrahend + " from " + minuend + " equals " + (minuend - subtrahend));
+    }
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.addition(13, 15);
+        calculator.subtraction(21, 32);
+    }
 }
